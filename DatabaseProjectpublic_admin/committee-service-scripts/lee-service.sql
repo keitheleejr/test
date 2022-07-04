d7284227abb1068b380c0192f626d8df1e19101d
keitheleejr <keilee@valdosta.edu>
2022-07-03 22:16:34 -0400

    commit

---
+++ DatabaseProjectpublic_admin/committee-service-scripts/lee-service.sql
@@ -0,0 +1,9 @@
+select * 
+from dbo.committee
+where 
+    Chair = 'Keith Lee' OR
+    CoChair = 'Keith Lee' OR
+    Methodologist = 'Keith Lee' OR
+    Member1 = 'Keith Lee' OR
+    Member2 = 'Keith Lee' OR
+    Member3 = 'Keith Lee'
